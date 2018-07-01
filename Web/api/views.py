5232ff4fdeea767d990ab51ad24b8be46917c896
Curtis Malainey <curtis@malainey.com>
2018-06-30 20:13:09 -0600

    Remove all things python or xbee

--- Web/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-from django.template import loader
-
-from .models import Coordinator
-
-
-def index(request):
-    return HttpResponse(request, "Test")
