d48e5e7909af746095ac6ee5b54dd10e9e53eb5a
Curtis Malainey <curtis@malainey.com>
2018-09-30 20:35:17 -0700

    Add Display API

--- Sensor-Relay/src/display-controller.cpp
+++ Sensor-Relay/src/display-controller.cpp
@@ -9,11 +9,40 @@
 
 Adafruit_AlphaNum4 display = Adafruit_AlphaNum4();
 
+static const char* display_strings[] = {
+    "ANT ",
+    "BOOT",
+    "CONF",
+    "DIAG",
+    "ME!!",
+    "USB ",
+    "WIFI",
+};
+
+void display_message(enum display_msgs idx) {
+    if (idx == DISPLAY_NONE) {
+        display.clear();
+        display.writeDisplay();
+        return;
+    }
+
+    display.writeDigitAscii(0, *display_strings[idx]);
+    display.writeDigitAscii(1, *(display_strings[idx]+1));
+    display.writeDigitAscii(2, *(display_strings[idx]+2));
+    display.writeDigitAscii(3, *(display_strings[idx]+3));
+    display.writeDisplay();
+}
+
+void display_error(enum display_msgs idx) {
+    display.blinkRate(DISPLAY_ERROR_BLINK_RATE);
+    display_message(idx);
+}
+
 void display_init() {
     DEBUG("Display: Init Started");
     display.begin(DISPLAY_ADDR);
     display.clear();
-    display.writeDisplay();
+    display_message(DISPLAY_BOOT);
     DEBUG("Display: Init Completed");
 }
 
--- Sensor-Relay/src/display-controller.h
+++ Sensor-Relay/src/display-controller.h
@@ -1,7 +1,22 @@
 #ifndef DISPLAY_h
 #define DISPLAY_h
 
+enum display_msgs {
+    DISPLAY_ANT,
+    DISPLAY_BOOT,
+    DISPLAY_CONF,
+    DISPLAY_DIAG,
+    DISPLAY_ME,
+    DISPLAY_USB,
+    DISPLAY_WIFI,
+    /* Count for compilation enforcement */
+    DISPLAY_COUNT,
+    DISPLAY_NONE,
+};
+
 void display_init(void);
 void display_tick(void);
+void display_message(enum display_msgs);
+void display_error(enum display_msgs);
 
 #endif // DISPLAY_h
