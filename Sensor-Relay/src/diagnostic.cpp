d06bb21cca7f667d313d5955204f91c41cae0aee
Curtis Malainey <curtis@malainey.com>
2019-07-10 21:32:05 -0700

    Add assert handling

--- Sensor-Relay/src/diagnostic.cpp
+++ Sensor-Relay/src/diagnostic.cpp
@@ -21,3 +21,15 @@ void diagnostic_init() {
 	}
     }
 }
+
+// handle diagnostic informations given by assertion and abort program execution:
+void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
+    // transmit diagnostic informations through serial link.
+    Serial.println(__func);
+    Serial.println(__file);
+    Serial.println(__lineno, DEC);
+    Serial.println(__sexp);
+    Serial.flush();
+    // abort program execution.
+    abort();
+}
--- Sensor-Relay/src/diagnostic.h
+++ Sensor-Relay/src/diagnostic.h
@@ -1,6 +1,11 @@
 #ifndef DIAGNOSTIC_h
 #define DIAGNOSTIC_h
 
+#define __ASSERT_USE_STDERR // trick arduino into using our assert handler
+#include <assert.h>
+
 void diagnostic_init(void);
+
+void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp);
 
 #endif // DIAGNOSTIC_h
