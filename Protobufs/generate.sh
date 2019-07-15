db3f599645a253c7c18893ae0a398f9aa91eb738
Curtis Malainey <curtis@malainey.com>
2019-07-15 00:04:59 -0700

    Use separate options file since protoc isn't playing nice

--- Protobufs/generate.sh
+++ Protobufs/generate.sh
@@ -1,2 +1,2 @@
 protoc -omessages.pb --csharp_out=../Server/Protobufs messages.proto
-python3 $(which nanopb_generator.py) messages.pb -D ../Sensor-Relay/src
+python3 $(which nanopb_generator.py) messages.pb -D ../Sensor-Relay/src -f messages.options
